65fec0f390ff32e2afa08022c3aab0769f85b233
Danilo <danilob@airp.ws>
2024-05-30 01:53:02 -0500

    2 Columnas en movil

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -4,7 +4,7 @@ import PokemonPreview from './PokemonPreview';
 
 const PokemonList = ({ pokemons, onClick }) => {
   return (
-    <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-4">
+    <section className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-4">
       {pokemons.map((pokemon, index) => (
         <PokemonPreview key={index} pokemon={pokemon} onClick={onClick} />
       ))}
